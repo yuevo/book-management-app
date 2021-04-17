dd0fc32d12aad14703e76a2b2813fe63e7ea05f4
yuevo <yuevo16@gmail.com>
2021-04-17 18:14:19 +0900

    components 追加

---
+++ src/router/AppRouter.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from '../components/Header';
+import AddBook from '../components/AddBook';
+import BooksList from '../components/BooksList';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+const AppRouter = () => {
+  const [books, setBooks] = useLocalStorage('books', []);
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <div className="main-content">
+          <Switch>
+            <Route
+              render={(props) => (
+                <BooksList {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/"
+              exact={true}
+            />
+            <Route
+              render={(props) => (
+                <AddBook {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/add"
+            />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
